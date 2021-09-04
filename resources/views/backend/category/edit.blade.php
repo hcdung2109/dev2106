728d398147ee43cdd24d66275bc0fef346620e25
hcdung2109 <hcdung2109@gmail.com>
2021-09-04 09:11:04 +0700

    fas

--- resources/views/backend/category/edit.blade.php
+++ resources/views/backend/category/edit.blade.php
@@ -4,7 +4,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Thêm Danh Mục <a href="{{ route('admin.category.index') }}" class="btn btn-success">Danh Sách</a>
+            Cập Nhật Danh Mục <a href="{{ route('admin.category.index') }}" class="btn btn-success">Danh Sách</a>
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -25,43 +25,45 @@
                     </div>
                     <!-- /.box-header -->
                     <!-- form start -->
-                    <form role="form" method="post" action="{{ route('admin.category.store') }}" enctype="multipart/form-data">
+                    <form enctype="multipart/form-data" role="form" method="post" action="{{ route('admin.category.update', ['id' => $data->id]) }}">
                         @csrf
+                        @method('PUT')
                         <div class="box-body">
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Tên</label>
-                                <input name="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="Nhập Tên">
+                                <input value="{{ $data->name }}" name="name" type="text" class="form-control" id="exampleInputEmail1" placeholder="Nhập Tên Thương hiệu">
                             </div>
 
                             <div class="form-group">
                                 <label for="exampleInputFile">Logo</label>
                                 <input name="image" type="file" id="exampleInputFile">
+                                <img style="margin-top: 15px; max-width: 200px;" src="{{ asset($data->image) }}" alt="">
                             </div>
 
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Danh Mục Cha</label>
                                 <select class="form-control" name="parent_id">
-                                    @foreach($data as $item)
-                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @foreach($list as $item)
+                                        <option {{ $item->id == $data->parent_id ? 'selected' : '' }} value="{{ $item->id }}">{{ $item->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
 
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Vị trí</label>
-                                <input name="position" type="number" class="form-control" id="exampleInputEmail1" min="1" value="1">
+                                <input value="{{ $data->position }}" name="position" type="number" class="form-control" id="exampleInputEmail1" min="1" value="1">
                             </div>
 
                             <div class="checkbox">
                                 <label>
-                                    <input type="checkbox" name="is_active" value="1"> Hiển thị
+                                    <input {{ $data->is_active == 1 ? 'checked' : '' }} type="checkbox" name="is_active" value="1"> Hiển thị
                                 </label>
                             </div>
                         </div>
                         <!-- /.box-body -->
 
                         <div class="box-footer">
-                            <button type="submit" class="btn btn-primary">Tạo</button>
+                            <button type="submit" class="btn btn-primary">Sửa</button>
                         </div>
                     </form>
                 </div>
