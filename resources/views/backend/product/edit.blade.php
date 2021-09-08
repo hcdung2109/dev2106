b1545044e4e0b9fd50eb0505006acb0739d6379d
hcdung2109 <hcdung2109@gmail.com>
2021-09-08 23:18:36 +0700

    update

--- resources/views/backend/product/edit.blade.php
+++ resources/views/backend/product/edit.blade.php
@@ -4,7 +4,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Thêm Sản Phẩm <a href="{{ route('admin.product.index') }}" class="btn btn-primary">Danh Sách</a>
+            Chỉnh sửa Sản Phẩm <a href="{{ route('admin.product.index') }}" class="btn btn-primary">Danh Sách</a>
         </h1>
     </section>
 
@@ -23,10 +23,10 @@
                 @endif
             </div>
 
-
             <!-- left column -->
-            <form role="form" action="{{ route('admin.product.store') }}" method="post" enctype="multipart/form-data">
+            <form role="form" action="{{ route('admin.product.update' , ['id' => $product->id ]) }}" method="post" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="col-md-9 col-lg-9">
                     <div class="box box-primary">
                         <div class="box-header with-border">
@@ -38,19 +38,21 @@
 
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Tên sản phẩm <span style="color: red">*</span></label>
-                                <input type="text" class="form-control" id="name" name="name">
+                                <input value="{{ $product->name }}" type="text" class="form-control" id="name" name="name">
                             </div>
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="exampleInputFile">Ảnh</label>
                                         <input type="file" class="" id="image" name="image">
+
+                                        <img width="200" src="{{ asset($product->image) }}" alt="ảnh cũ">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="exampleInputFile">Số lượng</label>
-                                        <input style="width: 100px" type="number" class="form-control" id="stock" name="stock" value="1" min="1">
+                                        <input  style="width: 100px" type="number" class="form-control" id="stock" name="stock" value="{{ $product->stock }}" min="1">
                                     </div>
                                 </div>
                             </div>
@@ -59,14 +61,14 @@
                                 <div class="col-lg-6">
                                     <div class="form-group">
                                         <label for="exampleInputFile">Giá gốc (vnđ)(<span style="color: red">*</span>)</label>
-                                        <input type="number" class="form-control" id="price" name="price"  min="0">
+                                        <input value="{{ $product->price }}" type="number" class="form-control" id="price" name="price"  min="0">
                                     </div>
                                 </div>
                                 <!-- /.col-lg-6 -->
                                 <div class="col-lg-6">
                                     <div class="form-group">
                                         <label for="exampleInputFile">Giá khuyến mại (vnđ)(<span style="color: red">*</span>)</label>
-                                        <input type="number" class="form-control" id="sale" name="sale"  min="0">
+                                        <input value="{{ $product->sale }}" type="number" class="form-control" id="sale" name="sale"  min="0">
                                     </div>
                                 </div>
                                 <!-- /.col-lg-6 -->
@@ -78,7 +80,7 @@
                                         <select class="form-control w-50" name="category_id">
                                             <option value="0">-- chọn Danh Mục --</option>
                                             @foreach($categories as $category)
-                                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                <option {{ $category->id == $product->category_id ? 'selected' : '' }} value="{{ $category->id }}">{{ $category->name }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -88,9 +90,9 @@
                                     <div class="form-group">
                                         <label>Thương Hiệu (<span style="color: red">*</span>)</label>
                                         <select class="form-control w-50" name="brand_id">
-                                            <option value="0">-- chọn Danh Mục --</option>
+                                            <option value="0">-- chọn Thương Hiệu --</option>
                                             @foreach($brands as $brand)
-                                                <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                                                <option {{ $brand->id == $product->brand_id ? 'selected' : '' }} value="{{ $brand->id }}">{{ $brand->name }}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -99,7 +101,7 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="exampleInputEmail1">Vị trí</label>
-                                        <input type="number" class="form-control w-50" id="position" name="position" value="0">
+                                        <input type="number" class="form-control w-50" id="position" name="position" value="{{ $product->position }}">
                                     </div>
                                 </div>
                             </div>
@@ -109,34 +111,34 @@
                             <div class="form-group">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" value="1" name="is_active" checked> <b>Hiển Thị</b>
+                                        <input {{ $product->is_active == 1 ? 'checked' : '' }} type="checkbox" value="1" name="is_active" checked> <b>Hiển Thị</b>
                                     </label>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" value="1" name="is_hot"> <b>Sản phẩm Hot</b>
+                                        <input {{ $product->is_hot == 1 ? 'checked' : '' }} type="checkbox" value="1" name="is_hot"> <b>Sản phẩm Hot</b>
                                     </label>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Liên kết (url) tùy chỉnh</label>
-                                <input type="text" class="form-control" id="url" name="url" placeholder="">
+                                <input value="{{ $product->url }}" type="text" class="form-control" id="url" name="url" placeholder="">
                             </div>
                             <div class="form-group">
                                 <label>Tóm tắt</label>
-                                <textarea id="editor2" name="summary" class="form-control" rows="10" ></textarea>
+                                <textarea id="editor2" name="summary" class="form-control" rows="10" >{{ $product->summary }}</textarea>
                             </div>
 
                             <div class="form-group">
                                 <label>Mô tả</label>
-                                <textarea id="editor1" name="description" class="form-control" rows="10" ></textarea>
+                                <textarea id="editor1" name="description" class="form-control" rows="10" >{{ $product->description }}</textarea>
                             </div>
                         </div>
                         <!-- /.box-body -->
                         <div class="box-footer">
-                            <button type="submit" class="btn btn-primary">Tạo</button>
+                            <button type="submit" class="btn btn-primary">Lưu</button>
                             <input type="reset" class="btn btn-default pull-right" value="Reset">
                         </div>
                     </div>
@@ -146,11 +148,11 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Meta Title</label>
-                        <textarea name="meta_title" class="form-control" rows="3" ></textarea>
+                        <textarea name="meta_title" class="form-control" rows="3" >{{ $product->meta_title }}</textarea>
                     </div>
                     <div class="form-group">
                         <label>Meta Description</label>
-                        <textarea name="meta_description" class="form-control" rows="5" ></textarea>
+                        <textarea name="meta_description" class="form-control" rows="5" >{{ $product->meta_description }}</textarea>
                     </div>
                 </div>
             </form>
